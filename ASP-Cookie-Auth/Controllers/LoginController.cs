fd1b6ed049f330e87038b9497e63cb8f8aae8f4f
Tturna <42873350+Tturna@users.noreply.github.com>
2024-03-21 20:20:08 +0200

    basic cookie auth

---
+++ ASP-Cookie-Auth/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Cookie_Auth.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Login(string username)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, username)
+        };
+        
+        if (username == "hermano")
+        {
+            claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+        }
+        
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+            AllowRefresh = true,
+            ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30),
+            IsPersistent = true
+        };
+
+        HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Index", "Home");
+    }
+
+    [Authorize]
+    public IActionResult Authorized()
+    {
+        return View();
+    }
+    
+    [Authorize(Roles = "Admin")]
+    public IActionResult Hidden()
+    {
+        return View();
+    }
+}
---
+++ ASP-Cookie-Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASP_Cookie_Auth.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
